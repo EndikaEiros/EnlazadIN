0d4d80d96662b9d5fc72a2600c753d34edabd6ea
Iker Valcarcel <ikervalcarcel@gmail.com>
2021-10-24 18:31:10 +0200

    Cosas

---
+++ app/modificar.php
@@ -0,0 +1,22 @@
+<?php
+    $id=$_GET['id'];
+    $nom=$_GET['nombre'];
+    $ape=$_GET['apellidos'];
+    $mail=$_GET['email'];
+    $telf=$_GET['telefono'];
+    $mensaje=$_GET['mensaje'];
+
+    $sql = "UPDATE comentarios SET NRECEP = '$nom',
+                                    ARECEP = '$ape',
+                                    ERECEP = '$mail',
+                                    Telefono = '$telf',
+                                    MSG = '$mensaje',
+                                    WHERE ID LIKE '$id';";
+    if (mysqli_query($conn, $sql)) {
+        echo "Record updated successfully";
+        echo '<script> window.location.href="/modificar-mensaje.php"</script>';
+    }else {
+        echo "Error updating record: " . $conn->error;
+      }
+    }
+?>
